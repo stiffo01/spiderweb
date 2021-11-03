4851443c4631250e9a6d43f43cd4eba682a342a7
Christoffer Hjerne <c.hjerne@gmail.com>
2021-11-03 12:32:56 +0100

    Make changes to script and add index.html

--- spiderweb.sh
+++ spiderweb.sh
@@ -1,8 +1,4 @@
-#while true;
-#  do echo -e "HTTP/1.1 200 OK\n\n$(iostat)" \
-#  | nc -l -k -p 8080 -q 1; 
-#done
 while true; do { \
-  echo -ne "HTTP/1.0 200 OK\r\nContent-Length: $(wc -c <index.htm)\r\n\r\n"; \
-  cat index.htm; } | nc -l -p 8080 ; \
+  echo -ne "HTTP/1.0 200 OK\r\nwebcontent: $(wc -c <index.html)\r\n\r\n"; \
+  cat index.html; } | nc -l -p 8080 -q 1; \
 done
