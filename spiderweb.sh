cc2538f5804a2b7f5dfcb7eec52558197a84efc9
Christoffer Hjerne <c.hjerne@gmail.com>
2021-11-05 12:11:21 +0100

    Change some files

--- spiderweb.sh
+++ spiderweb.sh
@@ -1,4 +1,4 @@
 while true; do { \
-  echo -ne "HTTP/1.0 200 OK\r\nwebcontent: $(wc -c <index.html)\r\n\r\n"; \
+  echo -ne "HTTP/1.0 200 OK\r\nContent-Length: $(wc -c <index.html)\r\n\r\n"; \
   cat index.html; } | nc -l -p 8080 -q 1; \
 done
