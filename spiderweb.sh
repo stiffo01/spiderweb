5b4ca5a145170dec30c9d4ce8b95f1cb11129085
Christoffer Hjerne <c.hjerne@gmail.com>
2021-11-12 14:51:02 +0100

    add index2.html and change output.txt and make some changes to spiderweb.sh

--- spiderweb.sh
+++ spiderweb.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 webfile="index.html"
 func1 () {
-  if [["$webfile"=='/']]
+  if [[ "$webfile" == '/' ]]
   then
     webfile="index.html"
+  else
+    webfile=$("$webfile" | tr -d "/")
   fi
 }
 func2 () {
-webfile=$(grep GET output.txt | cut -d' ' -f2); \
+webfile=$(head -1 output.txt | cut -d' ' -f2); \
 }
 while true; do { \
   echo -ne "HTTP/1.0 200 OK\r\nContent-Length: $(wc -c < $webfile)\r\n\r\n"; \
   cat $webfile; } | nc -l -p 80 -q 1; \
-  func2
-  func1
+  func2;\
+  func1;\
 done
