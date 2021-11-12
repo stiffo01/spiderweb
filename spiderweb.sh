fb6169e39b9ef87877096f67d74fdb6cb71c0bcf
Christoffer Hjerne <c.hjerne@gmail.com>
2021-11-12 14:03:49 +0100

    Make big changes to the spiderweb script and add output.txt file

--- spiderweb.sh
+++ spiderweb.sh
@@ -1,4 +1,17 @@
+#!/bin/bash
+webfile="index.html"
+func1 () {
+  if [["$webfile"=='/']]
+  then
+    webfile="index.html"
+  fi
+}
+func2 () {
+webfile=$(grep GET output.txt | cut -d' ' -f2); \
+}
 while true; do { \
-  echo -ne "HTTP/1.0 200 OK\r\nContent-Length: $(wc -c <index.html)\r\n\r\n"; \
-  cat index.html; } | nc -l -p 8080 -q 1; \
+  echo -ne "HTTP/1.0 200 OK\r\nContent-Length: $(wc -c < $webfile)\r\n\r\n"; \
+  cat $webfile; } | nc -l -p 80 -q 1; \
+  func2
+  func1
 done
