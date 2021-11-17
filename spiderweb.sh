48699b11cbd5c7b617ce78fcbf68de9b25b4d4d0
Christoffer Hjerne <c.hjerne@gmail.com>
2021-11-17 11:21:27 +0100

    Add big changes to spiderweb.sh

--- spiderweb.sh
+++ spiderweb.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
+#to run the script use: sudo bash spiderweb.sh | tee output.txt
 webfile="index.html"
+#func1 will search the output.txt file for what file is requested in the browser, if the browser is / (empty)
+#then it will replace the epmty / with index.html.
 func1 () {
+  if [[ "$webfile" == "null" ]]
+  then
+    echo "#do something"  
+  fi
+  webfile=$(head -1 output.txt | cut -d' ' -f2)
   if [[ "$webfile" == '/' ]]
   then
     webfile="index.html"
   else
-    webfile=$("$webfile" | tr -d "/")
+    webfile=$(echo "$webfile" | tr -d "/")
+    echo "$webfile"
   fi
 }
-func2 () {
-webfile=$(head -1 output.txt | cut -d' ' -f2); \
-}
 while true; do { \
-  echo -ne "HTTP/1.0 200 OK\r\nContent-Length: $(wc -c < $webfile)\r\n\r\n"; \
+  echo -ne "HTTP/1.0 200 OK\r\nContent-Length: $(wc -c < webfile=$(func1))\r\n\r\n"; \
   cat $webfile; } | nc -l -p 80 -q 1; \
-  func2;\
-  func1;\
 done
