8b0ab15ae06c9c1ebfb7b338cb2bd6fed440db56
Christoffer Hjerne <c.hjerne@gmail.com>
2021-11-17 13:52:48 +0100

    Update all files

--- spiderweb.sh
+++ spiderweb.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
+
 #to run the script use: sudo bash spiderweb.sh | tee output.txt
-webfile="index.html"
 #func1 will search the output.txt file for what file is requested in the browser, if the browser is / (empty)
 #then it will replace the epmty / with index.html.
-func1 () {
-  if [[ "$webfile" == "null" ]]
+
+func1 () { 
+  webfile=$(grep "GET" output.txt | tail -n1 | cut -d' ' -f2)
+  #this looks at the first line in output.txt and then sends this information to cut, cut will cut field 2 with the separator " " and print it to the variable $webfile
+  if [[ "$webfile" == '/' || -z $webfile ]]
   then
-    echo "#do something"  
-  fi
-  webfile=$(head -1 output.txt | cut -d' ' -f2)
-  if [[ "$webfile" == '/' ]]
-  then
-    webfile="index.html"
+    echo "index.html"
   else
-    webfile=$(echo "$webfile" | tr -d "/")
-    echo "$webfile"
+    echo "$webfile" | tr -d "/"
+    #this will remove the / from the filename. It is from the beginning /index.html like this, We want only the text
   fi
 }
-while true; do { \
-  echo -ne "HTTP/1.0 200 OK\r\nContent-Length: $(wc -c < webfile=$(func1))\r\n\r\n"; \
-  cat $webfile; } | nc -l -p 80 -q 1; \
+while true 
+do  
+  weblink=$(func1)
+  {
+  echo -ne "HTTP/1.0 200 OK\r\nContent-Length: $(wc -c < $weblink)"\r\n\r\n
+  cat $weblink; } | nc -l -p 80 -q 1
 done
